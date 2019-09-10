3525374492e2a5d26c52f7d7b8a632272e70b035
Byeonggeol Ha <byeonggeol98@gmail.com>
2019-09-10 16:17:41 +0900

    add webpack-dev-server example

---
+++ webpack-dev-server-example/src/view/HelloWorld.tsx
@@ -0,0 +1,18 @@
+// Created by Byeonggeol Ha on 2019-09-10
+import * as React from "react";
+
+interface Props {
+
+}
+
+interface State {
+
+}
+
+export default class HelloWorld extends React.Component<Props, State> {
+    render() {
+        return (
+            <div>Hello, World!</div>
+        );
+    }
+}

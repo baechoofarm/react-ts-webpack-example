aa632e2bbf74be17693476528e0da965ad9c2e41
Byeonggeol Ha <byeonggeol98@gmail.com>
2019-09-11 14:55:32 +0900

    add html-webpack-plugin example

---
+++ html-webpack-plugin-example/webpack.config.js
@@ -0,0 +1,34 @@
+// Created by Byeonggeol Ha on 2019-09-09
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve(__dirname, './src/main.tsx'),
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new webpack.ProgressPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+    devServer: {
+        contentBase: path.join(__dirname, './dist'),
+        compress: true,
+        port: 9000
+    },
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            loader: 'ts-loader',
+            exclude: [/node_modules/]
+        }]
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
+    }
+};
